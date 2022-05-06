479cf362829d01678327edcb8c221b7151c0d55f
PCesar45 <pacviu@gmail.com>
2022-05-05 18:59:39 -0600

    Validacion de contrase;a y toma de datos de los input

---
+++ bootstrap-5.0.2-dist/js/Registrarse.js
@@ -0,0 +1,11 @@
+
+function Registrarse() {
+  //document.getElementById("Contrase").addEventListener("invalid", ContraInvalida);  
+  var contr = document.getElementById("Contrase");
+  if (!contr.checkValidity()) {
+     alert('La contraseña debe tener mas de 8 caracteres y minimo 1 digito ,1 minuscula,1 mayuscula y un 1 caracter especial(!¡#¿?@)');
+  }
+  else{
+    href="../index.html"
+  }
+}

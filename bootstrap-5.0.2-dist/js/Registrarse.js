4e43e7e34feddfa715fe73cabdf850dba158e41c
PCesar45 <pacviu@gmail.com>
2022-05-05 22:31:47 -0600

    Menu y Tipo de cambio (interfaz)

--- bootstrap-5.0.2-dist/js/Registrarse.js
+++ bootstrap-5.0.2-dist/js/Registrarse.js
@@ -6,6 +6,6 @@ function Registrarse() {
      alert('La contraseña debe tener mas de 8 caracteres y minimo 1 digito ,1 minuscula,1 mayuscula y un 1 caracter especial(!¡#¿?@)');
   }
   else{
-    href="../index.html"
+    location.href="../index.html"
   }
 }
